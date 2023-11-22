ea59e337180848e47c8542913dc119d449b826dc
emunozestrada <emunozestrada@gmail.com>
2023-11-22 15:19:21 -0500

    correcciones

--- index.js
+++ index.js
@@ -1,61 +1,46 @@
-function enviarInformacion() {  
-   
-  let vendedor = document.getElementById("vendedor").value;  
-  let Aqua = document.getElementById("Aqua").value;
-  let Emocion= document.getElementById("Emocion").value;
-  let Alegria = document.getElementById("Alegria").value;
-  let Frescura = document.getElementById("Frescura").value;
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("ventasForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
 
-  let vendedor1 = "Juana";
-  let vendedor2 =  "Pedro";
- 
+    let vendedor = document.getElementById("vendedor").value;  
+    // let Aqua = parseInt(document.getElementById("Aqua").value) || 0;
+    // let Emocion = parseInt(document.getElementById("Emocion").value) || 0;
+    // let Alegria = parseInt(document.getElementById("Alegria").value) || 0;
+    // let Frescura = parseInt(document.getElementById("Frescura").value) || 0;
 
-  if(vendedor == "" || Aqua == ""|| Emocion ==""|| Alegria == ""|| Frescura ==""){
-  alert("Completa la información solicitada");
-  }else if (vendedor !== vendedor1 && vendedor !== vendedor2 && Aqua >= 0 && Emocion >= 0 && Alegria >= 0 && Frescura >= 0) {
-    alert("Debes ser un empleado vinculado a la empresa");          
-  }else{
-    let productos = {"Aqua": 200, "Emoción": 180, "Alegría": 160, "Frescura": 150 };
-    let vendedores = {"Juana": {"Aqua": 0, "Emoción": 0, "Alegría": 0, "Frescura": 0 }, "Pedro": { "Aqua": 0, "Emoción": 0, "Alegría": 0, "Frescura": 0 }};
-    
-    function agregarVenta(vendedor, producto, cantidad) {
-      vendedores[vendedor][producto] += cantidad;
-    }
+    let vendedor1 = "Juana";
+    let vendedor2 = "Pedro";
 
-    function calcularVentas(vendedor) {
-      let total = 0;
-      for (let producto in vendedores[vendedor]) {
-        total += vendedores[vendedor][producto] * productos[producto];
+    if (vendedor === "" ) {
+      alert("Completa la información solicitada");
+    } else if (vendedor !== vendedor1 && vendedor !== vendedor2) {
+      alert("Debes ser un empleado vinculado a la empresa");          
+    } else {
+      let productos = {"Aqua": 200, "Emocion": 180, "Alegria": 160, "Frescura": 150 };
+      let vendedores = {"Juana": {"Aqua": 0, "Emocion": 0, "Alegria": 0, "Frescura": 0 }, "Pedro": { "Aqua": 0, "Emocion": 0, "Alegria": 0, "Frescura": 0 }};
+      
+      function agregarVenta(vendedor, producto, cantidad) {      
+        vendedores[vendedor][producto] += cantidad;
       }
-      return total;
-    }
 
-    function empleadoDelMes() {
-      let maxVentas = 0;
-      let empleado = "";
-      for (let vendedor in vendedores) {
-        let ventas = calcularVentas(vendedor);
-        if (ventas > maxVentas) {
-          maxVentas = ventas;
-          empleado = vendedor;
-        } else if (ventas === maxVentas) {
-          empleado +=  vendedor;
+      function calcularVentas(vendedor) {
+        let total = 0;
+        for (let producto in vendedores[vendedor]) {
+          total += vendedores[vendedor][producto] * productos[producto];        
         }
-      }
-      return empleado;
-    }
-
-    document.getElementById("ventasForm").addEventListener("submit", function(event){
-      event.preventDefault();
-      let vendedor = document.getElementById("vendedor").value;
+        return total;          
+      }       
+      
       for (let producto in productos) {
-        let cantidad = parseInt(document.getElementById(producto));
+        console.log(producto);
+        let cantidad = parseInt(document.getElementById(producto).value) || 0;
         agregarVenta(vendedor, producto, cantidad);
       }
-      console.log(empleadoDelMes()); // Imprime el empleado del mes
-    });
-  }   
-}    
+      let ventas = calcularVentas(vendedor);
+      alert("El vendedor " + vendedor + " vendió un total de " + ventas + " USD."); 
+    }
+  });
+});
 
   
 
